cf7aec966cfbc187ecaf86a881b0eed9d6733a5a
Wesley <wesleyjr0101@gmail.com>
2018-10-26 20:25:17 -0300

    adding first scrapy spider

---
+++ Building_Simple_Spider/quotes_spider/quotes_spider/spiders/quotes.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    #this Request is called when there are no explicity callback
+    def parse(self, response):
+        h1_tag = response.xpath("//h1/a/text()").extract_first()
+        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        
+        yield {
+                'H1 Tag': h1_tag,
+                'Tags': tags
+                }
